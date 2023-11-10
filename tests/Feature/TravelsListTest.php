755e5d442808448e8d094f702a887b725e43840b
Rogerkorantenng <rogerkorantenng@gmail.com>
2023-11-10 17:00:55 +0000

    initial commit

---
+++ tests/Feature/TravelsListTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Travel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TravelsListTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_travels_list_returns_paginated_data_correctly(): void
+    {
+        Travel::factory()->count(16)->create(['is_public' => true]);
+
+        $response = $this->get('/api/v1/travels');
+
+        $response->assertStatus(200);
+
+        $response->assertJsonCount(15, 'data');
+        $response->assertJsonPath('meta.last_page', 2);
+    }
+
+    public function test_travels_list_returns_only_public_records()
+    {
+        $publicTravel = Travel::factory()->create(['is_public' => true]);
+        Travel::factory()->create(['is_public' => false]);
+
+        $response = $this->get('/api/v1/travels');
+
+        $response->assertStatus(200);
+        $response->assertJsonPath('data.0.name', $publicTravel->name);
+        $response->assertJsonCount(1, 'data');
+    }
+}
